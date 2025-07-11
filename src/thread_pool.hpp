5347d901135a9e4839c841d10ce0bc2ba2436187
Dmitriy <Dmitry5335@yandex.ru>
2025-07-11 15:24:46 +0300

    [FEAT] Add ThreadPool constructor

--- src/thread_pool.hpp
+++ src/thread_pool.hpp
@@ -25,6 +25,23 @@ private:
   std::vector< std::thread > threads_;
   std::atomic< bool > isWork_;
   std::mutex tasksMutex_;
+
+  void run();
 };
+
+template < class Function, class... Args >
+ThreadPool< Function, Args... >::ThreadPool(size_t threadNum):
+  threadNum_(std::thread::hardware_concurrency() ? std::min(std::thread::hardware_concurrency(), threadNum) : threadNum),
+  tasks_(),
+  threads_(),
+  isWork_(true),
+  tasksMutex_()
+{
+  threads.reserve(threadNum_);
+  for (size_t i = 0; i != threadNum_; ++i)
+  {
+    threads_.emplace_back(ThreadPool::run);
+  }
+}
 
 #endif
