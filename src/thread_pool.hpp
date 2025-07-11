22d86b4598a2cd2b319018a620cf18c33000d5b0
Dmitriy <Dmitry5335@yandex.ru>
2025-07-11 15:04:47 +0300

    [FEAT] Add methods declaration and attributes to ThreadPool

--- src/thread_pool.hpp
+++ src/thread_pool.hpp
@@ -1,16 +1,30 @@
 #ifndef THREAD_POOL_HPP
 #define THREAD_POOL_HPP
 
+#include <atomic>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <future>
 #include <vector>
+#include <queue>
 
-template < class Function, class ...Args >
+template < class Function, class... Args >
 class ThreadPool
 {
+public:
   ThreadPool(size_t threadNum);
   ~ThreadPool();
+
+  auto submit(Function&& function, Args&&... args) -> std::future< std::invoke_result_t< Function, Args... > >;
+  void shutdown();
+  void wait();
+private:
+  size_t threadNum_;
+  std::queue< std::function< void() > > tasks_;
+  std::vector< std::thread > threads_;
+  std::atomic< bool > isWork_;
+  std::mutex tasksMutex_;
 };
 
 #endif
