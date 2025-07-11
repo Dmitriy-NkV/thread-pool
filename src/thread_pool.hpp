879396a7dfde566082420350761c731c569d3b25
Dmitriy <Dmitry5335@yandex.ru>
2025-07-11 16:20:01 +0300

    [FEAT] Add run method for threads and condition variable

--- src/thread_pool.hpp
+++ src/thread_pool.hpp
@@ -25,6 +25,7 @@ private:
   std::vector< std::thread > threads_;
   std::atomic< bool > isWork_;
   std::mutex tasksMutex_;
+  std::condition_variable tasksCV_;
 
   void run();
 };
@@ -35,12 +36,36 @@ ThreadPool< Function, Args... >::ThreadPool(size_t threadNum):
   tasks_(),
   threads_(),
   isWork_(true),
-  tasksMutex_()
+  tasksMutex_(),
+  tasksCV_()
 {
   threads.reserve(threadNum_);
   for (size_t i = 0; i != threadNum_; ++i)
   {
     threads_.emplace_back(ThreadPool::run);
+  }
+}
+
+template < class Function, class... Args >
+void ThreadPool< Function, Args... >::run()
+{
+  while (isWork_)
+  {
+    std::function< void() > task;
+    {
+      std::unique_lock< std::mutex > lock(tasksMutex_);
+      tasksCV_.wait(lock, [this]() -> bool { return !isWork_ || !tasks_.empty(); });
+      if (stop && tasks.empty())
+      {
+        return;
+      }
+      else
+      {
+        task = std::move(tasks_.front());
+        tasks_.pop();
+      }
+    }
+    task();
   }
 }
 
